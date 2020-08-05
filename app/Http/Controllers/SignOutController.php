da4b6a7c1992d1791b1e27262a7da1f11d088fb7
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-08-05 13:59:37 +0000

    Create logout function

---
+++ app/Http/Controllers/SignOutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+class SignOutController extends Controller
+{
+    public function logout()
+    {
+        Auth::logout();
+        return view('auth.login');
+    }
+
+}
