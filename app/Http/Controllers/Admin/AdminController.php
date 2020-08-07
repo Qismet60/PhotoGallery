09f6f182791d006c5f2c73cfa3f697778cc31d90
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-08-07 23:20:05 +0000

    Add new function on AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Studios;
 
 class AdminController extends Controller
 {
@@ -10,12 +11,21 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
+
     public function studios()
     {
         return view('admin.studios');
     }
-    public function admin(){
+
+    public function admin()
+    {
         return view('login');
+    }
+
+    public function studioWorks()
+    {
+        $studios = Studios::all();
+        return view('admin.studio_work',compact('studios'));
     }
 
 }
