59d895e82918ec199bc154b1fbbfcf6d3f44ee00
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-08-07 20:45:44 +0000

    Image Upload on Studios model

--- app/Studios.php
+++ app/Studios.php
@@ -9,8 +9,20 @@ class Studios extends Model
     protected $table = 'studios';
     protected $fillable = ['name', 'email', 'logo', 'number', 'address', 'about'];
 
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $logo = $model->logo->getClientOriginalName();
+            $model->logo->move('web/images/logo', $logo);
+
+            $model->logo = $logo;
+
+        });
+    }
+
     public function works()
     {
-        return $this->hasMany(StudioWork::class,'studios_id','id');
+        return $this->hasMany(StudioWork::class, 'studios_id', 'id');
     }
 }
