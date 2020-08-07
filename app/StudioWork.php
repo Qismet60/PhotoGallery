010b9785839e9b359ea2c02f40c4775946e1913c
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-08-07 23:20:41 +0000

    Fixed boot function on StudioWork

--- app/StudioWork.php
+++ app/StudioWork.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudioWork extends Model
 {
-    //
+    protected $table = 'studio_works';
+    protected $fillable = ['studios_id', 'works'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $works = $model->works->getClientOriginalName();
+            $model->works->move('web/images/works', $works);
+            $model->works = $works;
+
+        });
+    }
 }
